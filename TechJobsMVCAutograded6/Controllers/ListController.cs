40b1272703631ad4008e2bcadd59688c89db9d33
Airiell <airiellgarrett@gmail.com>
2023-04-07 16:24:13 -0500

    task2-complete

--- TechJobsMVCAutograded6/Controllers/ListController.cs
+++ TechJobsMVCAutograded6/Controllers/ListController.cs
@@ -43,7 +43,19 @@ public class ListController : Controller
 
     // TODO #2 - Complete the Jobs action method
     public IActionResult Jobs(string column, string value)
-    {
+    {  List<Job>jobs;
+        if (column == "all")
+        {
+            jobs = JobData.FindAll();
+            ViewBag.title = "All Jobs";
+        }
+        else
+        {
+            jobs = JobData.FindByColumnAndValue(column, value);
+            ViewBag.title = column;
+            ViewBag.jobs = jobs;
+        }
+
         return View();
     }
 }
